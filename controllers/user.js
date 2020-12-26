2819fdf6cff35c175a25c490a2b3b07cafd34751
Eustaquio Ferreira <eusferalves@gmail.com>
2020-12-26 16:15:21 -0300

    remoção do node modules

--- controllers/user.js
+++ controllers/user.js
@@ -1,12 +1,24 @@
 
 const User = require('../models/user');
+const bcrypt = require('bcrypt');
+
 module.exports = app => {
     app.get('/user', (req, res) => res.send('servidor rodando'));
 
     app.post('/user', (req, res) => {
         const user = req.body;
-        User.createUser(user);
-        res.send('salvou');
+        try {
+            var password = await bcrypt.hash(password, 10);
+            User.createUser({
+                name: user.name,
+                password: password,
+                nick: user.nick
+            });
+            res.send('salvou');
+    
+        } catch (error) {
 
+        }
+      
     })
 }
